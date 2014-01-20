21d2457cce486942bd45a24f958f442c939eac3d
Robert Stoll <rstoll@tutteli.ch>
2014-01-20 16:31:00 +0100

    initial commit including TypedArray

---
+++ TypedArray.php
@@ -0,0 +1,280 @@
+<?php
+/*
+ * Copyright 2013 Robert Stoll <rstoll@tutteli.ch>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * See file "LICENSE" in the root directory for more information.
+ *
+ */
+
+class TypeException extends Exception
+{   
+}
+
+interface Types
+{
+    const T_BOOL = 1;
+    const T_INT = 2;
+    const T_FLOAT = 3;
+    const T_STRING = 4;
+    const T_ARRAY = 5;
+    const T_RESOURCE = 6;
+    const T_CALLABLE = 7;
+    const T_CLASS_OR_INTERFACE = 8;
+
+}
+
+interface ITypeChecker
+{
+
+    function isValid($value);
+
+    function getTypeAsString();
+}
+
+class BoolTypeChecker implements ITypeChecker
+{
+    function getTypeAsString() {
+        return 'bool';
+    }
+
+    function isValid($value) {
+        return \is_bool($value);
+    }
+}
+
+class IntTypeChecker
+{
+    function getTypeAsString() {
+        return 'int';
+    }
+    function isValid($value) {
+        return \is_int($value);
+    }
+}
+
+class FloatTypeChecker
+{
+    function getTypeAsString() {
+        return 'float';
+    }
+    function isValid($value) {
+        return \is_float($value);
+    }
+}
+
+class StringTypeChecker
+{
+    function getTypeAsString() {
+        return 'string';
+    }
+    function isValid($value) {
+        return \is_string($value);
+    }
+}
+
+class ArrayTypeChecker
+{
+    function getTypeAsString() {
+        return 'array';
+    }
+    function isValid($value) {
+        return \is_array($value);
+    }
+}
+
+class ResourceTypeChecker
+{
+    function getTypeAsString() {
+        return 'resource';
+    }
+    function isValid($value) {
+        return \is_resource($value);
+    }
+}
+
+class CallableTypeChecker
+{
+    function getTypeAsString() {
+        return 'callable';
+    }
+    function isValid($value) {
+        return \is_callable($value);
+    }
+}
+
+class ObjectTypeChecker implements ITypeChecker
+{
+    private $classOrInterfaceName;
+    public function __construct($classOrInterfaceName) {
+        $this->classOrInterfaceName = $classOrInterfaceName;
+    }
+    function getTypeAsString() {
+        return $this->classOrInterfaceName;
+    }
+    function isValid($value) {
+        return \is_a($value, $this->classOrInterfaceName);
+    }
+}
+
+class TypeCheckerFactory
+{
+    private static $bool;
+    private static $int;
+    private static $float;
+    private static $string;
+    private static $array;
+    private static $resource;
+    private static $callable;
+
+    public static function build($typeAsInt, $classOrInterfaceName = null) {
+        switch ($typeAsInt) {
+            case Types::T_BOOL:
+                if (self::$bool === null) {
+                    self::$bool = new BoolTypeChecker();
+                }
+                return self::$bool;
+            case Types::T_INT:
+                if (self::$int === null) {
+                    self::$int = new IntTypeChecker();
+                }
+                return self::$int;
+            case Types::T_FLOAT:
+                if (self::$float === null) {
+                    self::$float = new FloatTypeChecker();
+                }
+                return self::$float;
+            case Types::T_STRING:
+                if (self::$string === null) {
+                    self::$string = new StringTypeChecker();
+                }
+                return self::$string;
+            case Types::T_ARRAY:
+                if (self::$array === null) {
+                    self::$array = new ArrayTypeChecker();
+                }
+                return self::$array;
+            case Types::T_RESOURCE:
+                if (self::$resource === null) {
+                    self::$resource = new ResourceTypeChecker();
+                }
+                return self::$resource;
+            case Types::T_CALLABLE:
+                 if (self::$callable === null) {
+                    self::$callable = new CallableTypeChecker();
+                }
+                return self::$callable;
+            case Types::T_CLASS_OR_INTERFACE:
+                if(!class_exists($classOrInterfaceName) && !interface_exists($classOrInterfaceName)){
+                    throw new TypeException('Cannot create a TypedArray because the class/interface '.$classOrInterfaceName.' does not exist');    
+                }
+                return new ObjectTypeChecker($classOrInterfaceName);
+            default:
+                throw new TypeException('The given typeAsInt '.$typeAsInt.' is not supported');
+        }
+    }
+}
+
+class TypedArray implements ArrayAccess, Countable
+{
+    /* @var $typeChecker ITypeChecker */
+
+    private $typeChecker;
+    private $typeAsInt;
+    private $arr;
+
+    public static function createFromArray($typeAsInt, array $arr, $classOrInterfaceName = null) {
+        $tarr = new TypedArray($typeAsInt, $classOrInterfaceName);
+        foreach ($arr as $k => $v) {
+            $tarr[$k] = $v;
+        }
+        return $tarr;
+    }
+
+    public function __construct($typeAsInt, $classOrInterfaceName = null) {
+        $this->typeChecker = TypeCheckerFactory::build($typeAsInt, $classOrInterfaceName);
+    }
+
+    public function checkIsSameType($typeAsInt, $classOrInterfaceName = null) {
+        $arrTypeAsInt = $this->GetTypeAsInt();
+        if ($arrTypeAsInt !== $typeAsInt) {
+            switch ($typeAsInt) {
+                case Types::T_BOOL:
+                    $type = 'bool';
+                    break;
+                case Types::T_INT:
+                    $type = 'int';
+                    break;
+                case Types::T_FLOAT:
+                    $type = 'float';
+                    break;
+                case Types::T_STRING:
+                    $type = 'string';
+                    break;
+                case Types::T_ARRAY:
+                    $type = 'array';
+                    break;
+                case Types::T_RESOURCE:
+                    $type = 'resource';
+                    break;
+                case Types::T_CALLABLE:
+                    $type = 'callable';
+                    break;
+                case Types::T_CLASS_OR_INTERFACE:
+                    if($classOrInterfaceName === null){
+                        throw new TypeException('The given TypedArray is of type '.$this->getTypeAsString().' and not of type class/interface ($classOrInterfaceName was missing, cannot provide a more precise error message)');
+                    }
+                    $type = $classOrInterfaceName;
+                    break;
+                default:
+                    throw new TypeException('The given typeAsInt '.$typeAsInt.' is not supported');
+            }
+            throw new TypeException('The given TypedArray is of type '.$this->getTypeAsString().' and not of type '.$type);
+        }else if($arrTypeAsInt == Types::T_CLASS_OR_INTERFACE){
+            $type = $this->getTypeAsString();
+            if($type != $classOrInterfaceName){
+                throw new TypeException('The given TypedArray is of type '.$type.' and not of type '.$classOrInterfaceName);
+            }
+        }
+    }
+
+    public function isSameType($typeAsInt) {
+        return $this->GetTypeAsInt() === $typeAsInt;
+    }
+
+    public function getTypeAsString() {
+        return $this->typeChecker->getTypeAsString();
+    }
+
+    public function getTypeAsInt() {
+        return $this->typeAsInt;
+    }
+
+    public function isValid($value) {
+        return $this->typeChecker->isValid($value);
+    }
+
+    public function offsetSet($offset, $value) {
+        if ($this->isValid($value)) {
+            $arr[] = $value;
+        } else {
+            throw new TypeException('The given value was not of type '.$this->getTypeAsString().': '.var_export($value, true));
+        }
+    }
+
+    public function offsetExists($offset) {
+        return isset($this->arr[$offset]);
+    }
+
+    public function offsetGet($offset) {
+        return $this->arr[$offset];
+    }
+
+    public function offsetUnset($offset) {
+        $this->arr[$offset];
+    }
+
+    public function count() {
+        return \count($this->arr);
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,116 @@
+<?php
+/*
+ * Copyright 2013 Robert Stoll <rstoll@tutteli.ch>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * See file "LICENSE" in the root directory for more information.
+ *
+ */
+
+include __DIR__.'\TypedArray.php';
+
+function test($fnc) {
+    try {
+        $fnc();
+    } catch (TypeException $ex) {
+        echo '<div style="border:1px dashed #F00; padding:10px; background-color:#FDB0C1;margin-top:5px;margin-bottom:5px;">'.
+        '<span style="font-weight:bold">'.$ex->getMessage().'</span><br/>'.\str_replace("\n",'<br/>',$ex->getTraceAsString()).'</div>';
+    }
+}
+echo 'TypedArray of type bool:<br/>';
+test(function() {
+            $tarr = new TypedArray(Types::T_BOOL);
+            $tarr[] = true;
+            $tarr[] = false;
+
+            $tarr[] = 1;
+        });
+
+echo 'TypedArray of type int:<br/>';
+test(function() {
+            $tarr = new TypedArray(Types::T_INT);
+            $tarr[] = 1;
+            $tarr[] = 2;
+
+            $tarr[] = 3.0;
+        });
+        
+echo 'TypedArray of type float:<br/>';
+test(function() {
+            $tarr = new TypedArray(Types::T_FLOAT);
+            $tarr[] = 1.2;
+            $tarr[] = 2.4;
+
+            $tarr[] = 'a';
+        });
+
+echo 'TypedArray of type string:<br/>';
+test(function() {
+            $tarr = new TypedArray(Types::T_STRING);
+            $tarr[] = 'a';
+            $tarr[] = "b";
+
+            $tarr[] = 1;
+        });
+
+echo 'TypedArray of type resource:<br/>';
+test(function() {
+            $tarr = new TypedArray(Types::T_RESOURCE);
+            $tarr[] = fopen('TypedArray.php', 'r');
+
+            $tarr[] = 'a';
+        });
+        
+echo 'TypedArray of type callable:<br/>';
+test(function() {
+            $tarr = new TypedArray(Types::T_CALLABLE);
+            $tarr[] = array($tarr, "isValid");
+            $tarr[] = array($tarr, "getTypeAsInt");
+
+            $tarr[] = 'a';
+        });
+        
+echo 'TypedArray of type class/interface:<br/>';
+test(function() {
+            $tarr = new TypedArray(Types::T_CLASS_OR_INTERFACE, 'TypedArray');
+            $tarr[] = new TypedArray(Types::T_BOOL);
+            $tarr[] = new TypedArray(Types::T_FLOAT);
+
+            $tarr[] = 'a';
+        });
+
+echo '<br/><br/>type hint test';        
+        
+class Foo{}
+class Bar{}
+        
+function foo(TypedArray $arr){
+    $arr->checkIsSameType(Types::T_CLASS_OR_INTERFACE, "Foo");
+}
+
+test(function(){
+    foo(new TypedArray(Types::T_BOOL));
+});
+test(function(){
+    foo(new TypedArray(Types::T_CLASS_OR_INTERFACE, "NonExistingClass"));
+});
+test(function(){
+    foo(new TypedArray(Types::T_CLASS_OR_INTERFACE, "Bar"));
+});
+
+
+echo 'Array initialisation:<br/>';
+
+//initialisation of a TypedArray - almost like a normal array
+$typedArray = TypedArray::createFromArray(Types::T_CLASS_OR_INTERFACE, [new Foo(), new Foo()], "Foo");
+var_dump($typedArray);
+$typedArray = TypedArray::createFromArray(Types::T_CLASS_OR_INTERFACE, ['a' => new Foo(), 2 => new Foo()], "Foo");
+var_dump($typedArray);
+
+echo 'Reference test:<br/>';
+
+$typedArray = new TypedArray(Types::T_CLASS_OR_INTERFACE, "Foo");
+$foo = new Foo();
+$typedArray[] = &$foo;
+var_dump($typedArray);
+
+?>
